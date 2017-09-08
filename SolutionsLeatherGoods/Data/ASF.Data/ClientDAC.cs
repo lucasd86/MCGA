e21c141cb5829b20f54956df1a81ef41365ddf5d
lucasd86 <lucas.m.davy@gmail.com>
2017-09-07 23:32:04 -0300

    Modificacion cadena string - Agregado de ABM Category

---
+++ SolutionsLeatherGoods/Data/ASF.Data/ClientDAC.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using ASF.Entities;
+
+namespace ASF.Data
+{
+    public class ClientDAC : DataAccessComponent
+
+    {
+        public Client Create(Client client)
+        {
+           
+            {
+               
+            }
+
+            return client;
+        }
+
+       
+        public void UpdateById(Client client)
+        {
+            
+            
+           
+        }
+
+       
+        public void DeleteById(int id)
+        {
+            const string sqlStatement = "DELETE dbo.Category WHERE [Id]=@Id ";
+            var db = DatabaseFactory.CreateDatabase(ConnectionName);
+            using (var cmd = db.GetSqlStringCommand(sqlStatement))
+            {
+                db.AddInParameter(cmd, "@Id", DbType.Int32, id);
+                db.ExecuteNonQuery(cmd);
+            }
+        }
+
+        
+        public Client SelectById(int id)
+        {
+         
+
+            Client client = null;
+           
+            {
+             
+            }
+
+            return client;
+        }
+
+      
+        public List<Client> Select()
+        {
+           
+            var result = new List<Client>();
+            return result;
+        }
+
+        
+    }
+}
--- SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Areas/Category/Controllers/CategoryController.cs
+++ SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Areas/Category/Controllers/CategoryController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASF.Entities;
 
 namespace ASF.UI.WbSite.Areas.Category.Controllers
 {
     public class CategoryController : Controller
+
     {
+        
+
+
         // GET: Category/Category
         public ActionResult Index()
         {
-            return View();
+            ASF.Business.CategoryBusiness CatBus = new ASF.Business.CategoryBusiness();
+            Process.CategoryProcess CatProc = new Process.CategoryProcess();
+            Entities.Category cat = new Entities.Category();
+            
+            List<Entities.Category> Listacat = new List<Entities.Category>();
+            Listacat = CatBus.All();
+
+            
+            return View(Listacat);
         }
     }
 }
